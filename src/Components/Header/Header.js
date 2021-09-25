7a53aeb6b73d077797a8128e40b561ae551ee19e
unknown <sharifmahmud825@gmail.com>
2021-09-26 01:14:09 +0600

    header section added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <div className='text-center header py-4'>
+      <h1>World's Billionaires List</h1>
+      <h4>The Richest in 2021</h4>
+      <p>Source: Forbe's & Created By Sharif Mahmud</p>
+    </div>
+  );
+};
+
+export default Header;
